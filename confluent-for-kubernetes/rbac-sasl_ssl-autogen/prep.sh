bbd4027217a448b9c95d9b51cb85b108dd982624
Pere Urbon <pere@confluent.io>
2023-04-03 12:15:17 +0200

    inspect and adapt

--- confluent-for-kubernetes/rbac-sasl_ssl-autogen/prep.sh
+++ confluent-for-kubernetes/rbac-sasl_ssl-autogen/prep.sh
@@ -7,3 +7,13 @@ kubectl apply -f cfk/confluent-for-kubernetes/crds
 
 kubectl create namespace kafka-sasl-ssl
 kubectl apply -f cfk-permissions.yaml
+
+
+###
+helm upgrade --install operator confluentinc/confluent-for-kubernetes --namespace kafka-sasl-ssl
+kubens  kafka-sasl-ssl
+
+helm upgrade --install -f ../assets/openldap/ldaps-rbac.yaml test-ldap ../assets/openldap 
+kubectl  exec -it ldap-0 -- bash
+
+ldapsearch -LLL -x -H ldap://ldap.kafka-sasl-ssl.svc.cluster.local:389 -b 'dc=test,dc=com' -D "cn=mds,dc=test,dc=com" -w 'Developer!'
--- confluent-for-kubernetes/rbac-sasl_ssl-autogen/scripts/prepare-environment.sh
+++ confluent-for-kubernetes/rbac-sasl_ssl-autogen/scripts/prepare-environment.sh
@@ -11,6 +11,8 @@ helm upgrade --install operator confluentinc/confluent-for-kubernetes \
 ## LDAP
 helm upgrade --install -f ../assets/openldap/ldaps-rbac.yaml test-ldap ../assets/openldap --namespace kafka-sasl-ssl
 
+kubectl apply -f cfk-permissions.yaml
+
 # kubectl exec -it ldap-0 -- bash
 # ldapsearch -LLL -x -H ldap://ldap.confluent-sssl.svc.cluster.local:389 -b 'dc=test,dc=com' -D "cn=mds,dc=test,dc=com" -w 'Developer!'
 ##
